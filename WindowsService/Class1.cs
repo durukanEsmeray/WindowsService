4b8e6e0d198647520238f2420ce0518ac46d1e0d
durukanEsmeray <durukanesmeray@gmail.com>
2023-03-20 17:28:48 +0300

    Add project files.

---
+++ WindowsService/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public class Class1
+    {
+        // WEB SERVİS NEDİR ?
+        // gündelik hayatta, iş yerlerimizde bazı şeyler periyetik olarak arka tarafta işler ve kullanıcılara aynı zamanda mailler ya da farklı şekilde kayıtlar ulaşır. Bunları aslında bir insan hep aynı zamanda yapmaz. Bunları arkada sistem içerisinde yazmış olduğumuz Windows Servislerimiz yapar.
+        // Örnek olarak birine hergün belirli bir saatte mail gider. Mailin içinde bir rapor vardır. Ve o rapor hergün aynı saatte gitmeye devam eder.
+        // Bu işlemler servis durdurulana kadar veya makina kapatılana kadar devam eder.
+        // Aslında arka planda çalışan bir mekanizma var, bu mekanizmanın herhangi bir kullanıcı arayüzü yok. tamamıyle windows işletim sisteminin içerisine entegre olan ve arka tarafta belirtmiş olduğumuz zaman periyodu geldiğinde çalışan yapılara windows servisler denir.
+
+
+    }
+}
