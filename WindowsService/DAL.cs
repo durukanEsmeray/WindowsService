4378c25eb12f3d929f100552b2245ce45a03c7f1
durukanEsmeray <durukanesmeray@gmail.com>
2023-03-21 11:04:45 +0300

    Data Access Layer sınıfı oluşturuldu

---
+++ WindowsService/DAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public class DAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        int ReturnValues;
+
+        public DAL()
+        {
+            con = new SqlConnection("data source=DESKTOP-I299EIT\\SQLEXPRESS; initial catalog =OlayGunlugu; user Id = sa; password=123456;");
+        }
+
+        public int OlayGunluguYeniKayit(string Olay, DateTime OlusturmaTarihi)
+        {
+            try 
+            {
+                cmd = new SqlCommand("insert into OlayGunlugu (Olay, OlusturmaTarihi) values (@Olay, @OlusturmaTarihi)", con);
+                cmd.Parameters.Add("@Olay", SqlDbType.NVarChar).Value = Olay;
+                cmd.Parameters.Add("@OlusturmaTarihi", SqlDbType.DateTime).Value = OlusturmaTarihi;
+                con.Open();
+                ReturnValues = cmd.ExecuteNonQuery();
+            }
+            catch (Exception) 
+            {
+ 
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return ReturnValues;
+
+
+
+        }
+
+        
+    }
+}
--- WindowsService/Servisim.cs
+++ WindowsService/Servisim.cs
@@ -7,48 +7,65 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers; // servisin çalışma periyodunu belirleyeceğiz
 
 namespace WindowsService
 {
     public partial class Servisim : ServiceBase
     {
+        public Timer t;
         public Servisim()
         {
             InitializeComponent();
+            t = new Timer(30000); //30 saniye
+            t.Elapsed += T_Elapsed;
         }
+
+        private void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //burada SQL e bağlanıp kayıt oluşturacağız.
+            DAL D = new DAL();
+            D.OlayGunluguYeniKayit("Servis çalıştı", DateTime.Now);
+        }
+
         /*
-         * Başlama
-         * Pause
-         * Continue
-         * Stop
-         * OnShutDown
-         * 
-         */
+* Başlama
+* Pause
+* Continue
+* Stop
+* OnShutDown
+* 
+*/
 
 
         protected override void OnStart(string[] args) //başlama
         {
             //servis çalıştığı zaman çalışacak olan kod bloğu
+            t.Start();
         }
 
         protected override void OnPause()
         {
             //servis pause edildiği zaman çalışacak olan metod
+            t.Stop();
         }
 
         protected override void OnContinue()
         {
             //pause işleminden devam ettirildiği zaman çalışacak olan metod.
+            t.Start();
         }
 
         protected override void OnStop()
         {
             //Servis durdurulduğu zaman çalışacak olan metod.
+            t.Stop();
         }
 
         protected override void OnShutdown()
         {
             //servis shutdown edildiği zaman çalışacak olan metod.
+            t.Stop();
         }
     }
 }
